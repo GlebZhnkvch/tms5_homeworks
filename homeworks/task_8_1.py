6ceccb9401b6f19c2e4d3c21ab757065b7619003
GlebZhnkvch <71846540+GlebZhnkvch@users.noreply.github.com>
2020-10-05 18:24:36 +0200

    Delete task_8_1.py

--- homeworks/task_8_1.py
+++
@@ -1,9 +0,0 @@
-def function_one_arg(arg_list_):
-    try:
-        arg_list_ = set(arg_list_)  # Convert to set
-        return arg_list_
-    except TypeError:
-        return (f'Please, enter the list, you entered: {arg_list_}')
-
-
-print(function_one_arg([1, 2, 3, 5, 1]))
